fa6f64d4f9231c3dfd0bee5179668bb03d460624
Evan Pete Walsh <epwalsh10@gmail.com>
2020-03-23 18:00:31 -0700

    Improve macro and docs (#2)
    * refactor macro
    
    * clean up
    
    * updates
    
    * uptick version
    
    * improve example

--- examples/example.rs
+++ examples/example.rs
@@ -1,20 +1,50 @@
+// This example shows how you could use `batched_fn!` with a deep learing model
+// to get batched inference.
+//
+// Here our `Model` class just takes a vector of i32s and doubles them in order
+// to simulate some computation.
+
+// For lazily loading a static reference to a model instance.
 use batched_fn::batched_fn;
+use once_cell::sync::Lazy;
 use tokio::time::{self, Duration};
 
-async fn double(x: i32) -> i32 {
-    let batched = batched_fn! {
-        |batch: Vec<i32>| -> Vec<i32> {
-            let mut out = Vec::with_capacity(batch.len());
-            for x in &batch {
-                out.push(x * 2);
-            }
-            println!("Processed batch {:?} -> {:?}", batch, out);
-            out
+// `Batch` could be anything that implements the `batched_fn::Batch` trait.
+// We'll just use a Vec<T> type since the `Batch` trait has a blanket implementation
+// for Vecs.
+type Batch<T> = Vec<T>;
+
+type Input = i32;
+
+type Output = i32;
+
+struct Model {}
+
+// This is our stupid model that just doubles the inputs.
+impl Model {
+    fn predict(&self, batch: Batch<Input>) -> Batch<Output> {
+        batch.iter().map(|input| input * 2).collect()
+    }
+
+    fn load() -> Self {
+        Self {}
+    }
+}
+
+// Load a model instance.
+static MODEL: Lazy<Model> = Lazy::new(Model::load);
+
+async fn predict_for_single_input(input: Input) -> Output {
+    let batch_predict = batched_fn! {
+        |batch: Batch<Input>| -> Batch<Output> {
+            let output = MODEL.predict(batch.clone());
+            println!("Processed batch {:?} -> {:?}", batch, output);
+            output
         },
         max_batch_size = 4,
         delay = 50,
     };
-    batched(x).await
+    batch_predict(input).await
 }
 
 #[tokio::main]
@@ -22,7 +52,7 @@ async fn main() {
     let mut handles = vec![];
 
     handles.push(tokio::spawn(async move {
-        double(0).await;
+        predict_for_single_input(0).await;
     }));
 
     // Pause for a moment before firing off some more tasks.
@@ -30,7 +60,7 @@ async fn main() {
 
     for i in 1..10 {
         handles.push(tokio::spawn(async move {
-            double(i).await;
+            predict_for_single_input(i).await;
         }));
     }
 
